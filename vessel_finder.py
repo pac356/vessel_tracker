1a5b09446a263da611662976d81d5295544f30bc
pac356 <142249201+pac356@users.noreply.github.com>
2023-08-17 20:21:20 +0200

    Update vessel_finder.py

--- vessel_finder.py
+++ vessel_finder.py
@@ -5,54 +5,58 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime, timezone
 
-async def connect_ais_stream(mmsi_filter):
+async def connect_ais_stream(mmsi_filter=None, bounding_box=None):
+
+    subscribe_message = {
+        "APIKey": "1b7af1885ca28ca99cc1a166c9a6aa5983dee696",
+        "FilterMessageTypes": ["PositionReport"]
+    }
+
+    if mmsi_filter:
+        subscribe_message["FiltersShipMMSI"] = mmsi_filter
+
+    if bounding_box:
+        subscribe_message["BoundingBoxes"] = [bounding_box]
 
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
-        subscribe_message = {
-            "APIKey": "c0ff27fded31c9c0ed390b7dcedae4e40daf0c62",
-            "BoundingBoxes": [[[-90, -180], [90, 180]]],
-            "FiltersShipMMSI": mmsi_filter,
-            "FilterMessageTypes": ["PositionReport"]
-        }
-
         subscribe_message_json = json.dumps(subscribe_message)
         await websocket.send(subscribe_message_json)
 
-        async for message_json in websocket:
-            message = json.loads(message_json)
-            message_type = message["MessageType"]
+        # Assuming multiple messages can be received for bounding box query
+        positions = []
+        try:
+            async for message_json in asyncio.wait_for(websocket, timeout=60):
+                message = json.loads(message_json)
+                if message["MessageType"] == "PositionReport":
+                    ais_message = message['Message']['PositionReport']
+                    positions.append((ais_message['Latitude'], ais_message['Longitude']))
+        except asyncio.TimeoutError:
+            pass
 
-            if message_type == "PositionReport":
-                ais_message = message['Message']['PositionReport']
-                return ais_message['Latitude'], ais_message['Longitude']
-
-async def main_async(mmsi_filters):
-    tasks = [connect_ais_stream([mmsi_filter]) for mmsi_filter in mmsi_filters]
-    results = await asyncio.gather(*tasks, return_exceptions=True)
-    
-    latitudes = []
-    longitudes = []
-
-    for index, result in enumerate(results):
-        if isinstance(result, Exception):
-            st.write(f"Failed to fetch location for MMSI {mmsi_filters[index]}. Error: {result}")
-        else:
-            latitude, longitude = result
-            latitudes.append(latitude)
-            longitudes.append(longitude)
-            st.write(f"MMSI: {mmsi_filters[index]} - Latitude: {round(latitude, 5)} - Longitude: {round(longitude, 5)}")
-    
-    if latitudes and longitudes:
-        data = {'LATITUDE': latitudes, 'LONGITUDE': longitudes}
-        st.map(pd.DataFrame(data))
+        return positions
 
 def main():
     st.title("Vessel Location Tracker")
-    mmsi_input = st.text_input("Enter Vessel MMSI (comma-separated for multiple):", "")
-    mmsi_filters = [x.strip() for x in mmsi_input.split(',')] if mmsi_input else []
 
-    if st.button("Track Boats"):
-        asyncio.run(main_async(mmsi_filters))
+    mode = st.radio("Choose a query mode", ["By MMSI", "By Bounding Box"])
+
+    if mode == "By MMSI":
+        mmsi_filter = st.text_input("Enter Vessel MMSI:", "")
+        if st.button("Track Boat"):
+            positions = asyncio.run(connect_ais_stream([mmsi_filter]))
+            data = {'LATITUDE': [pos[0] for pos in positions], 'LONGITUDE': [pos[1] for pos in positions]}
+            st.map(pd.DataFrame(data))
+    
+    elif mode == "By Bounding Box":
+        min_lat = st.number_input("Enter Minimum Latitude:", value=-90.0, max_value=90.0)
+        max_lat = st.number_input("Enter Maximum Latitude:", value=90.0, max_value=90.0)
+        min_lon = st.number_input("Enter Minimum Longitude:", value=-180.0, max_value=180.0)
+        max_lon = st.number_input("Enter Maximum Longitude:", value=180.0, max_value=180.0)
+
+        if st.button("Fetch Vessels"):
+            positions = asyncio.run(connect_ais_stream(bounding_box=[[min_lat, min_lon], [max_lat, max_lon]]))
+            data = {'LATITUDE': [pos[0] for pos in positions], 'LONGITUDE': [pos[1] for pos in positions]}
+            st.map(pd.DataFrame(data))
 
 if __name__ == "__main__":
     main()
